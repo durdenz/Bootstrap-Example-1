99a3d6ac695530a6a9be6bf0a1da631cc45d223d
George Durden <george@durden.com>
2025-04-27 13:20:08 -0500

    Add GSAP Animation for cards

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    // gsap code here!
+      gsap.to(".card", {
+      rotation : 360,
+      duration : 2,
+      repeat: -1,
+      repeatDelay: .25,
+      })
+   });
